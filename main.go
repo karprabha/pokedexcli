06882871a4375387b167ad01a2a28e486b19c025
Prabhakar Yadav <developer.prabhakaryadav@gmail.com>
2025-06-26 22:20:53 +0530

    add user input repl

--- main.go
+++ main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 )
 
+func cleanInput(text string) []string {
+	words := strings.Fields(text)
+	for i := range words {
+		words[i] = strings.ToLower(words[i])
+	}
+	return words
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+
+		scanner.Scan()
+		text := scanner.Text()
+		cleaned := cleanInput(text)
+		firstWord := cleaned[0]
+
+		fmt.Println("Your command was:", firstWord)
+	}
 }
---
+++ repl_test.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "  HELLO  WORLD  ",
+			expected: []string{"hello", "world"},
+		},
+		{
+			input:    "  hello  world  ",
+			expected: []string{"hello", "world"},
+		},
+	}
+
+	for _, c := range cases {
+		actual := cleanInput(c.input)
+		for i := range actual {
+			word := actual[i]
+			expectedWord := c.expected[i]
+
+			if word != expectedWord {
+				t.Errorf("Expected %s, got %s", expectedWord, word)
+			}
+		}
+	}
+}
